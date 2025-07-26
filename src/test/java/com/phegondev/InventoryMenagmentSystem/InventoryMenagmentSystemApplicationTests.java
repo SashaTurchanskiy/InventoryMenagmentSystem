d4ddfbeb0f06892e777ffc26d8a81ef8f32066af
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-07-26 09:30:54 +0300

    Set up project, add db connection and added maven dependency

---
+++ src/test/java/com/phegondev/InventoryMenagmentSystem/InventoryMenagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phegondev.InventoryMenagmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMenagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
